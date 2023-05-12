2a9f67514c79e4c1b6678c238356d5bc3a07b011
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-05-12 11:07:16 -0600

    primer commit de archivos java

---
+++ java/java-hola-mundo/HolaMundo.java
@@ -0,0 +1,14 @@
+public class HolaMundo{
+  public static void main (String args[]){
+     //Este es un comentario de linea
+     /*Este es un comentario de bloque
+      A continuacion se enviará un mensaje 
+      en la consola.
+*/
+
+    System.out.println("Hola mundo");
+
+
+   }
+
+}
