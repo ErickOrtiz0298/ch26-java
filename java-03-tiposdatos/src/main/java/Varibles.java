9291fb21c67acb399042df305f362eb699ccf52a
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-05-12 11:38:33 -0600

    mejora de navegacion entre proyectos y presentacion en git

--- java-03-tiposdatos/src/main/java/Varibles.java
+++ java-03-tiposdatos/src/main/java/Varibles.java
@@ -1,5 +1,15 @@
 public class Varibles {
     public static void main(String[] args){
-        
+        /*
+        Reglas para el nombre de las variables:
+        -Se distingue entre mayusculas y minusculas
+        -La longitud del nombre es ilimitada
+        -Puedes utilizar cualquier caracter unicode
+        -La variable puede comenzar con:
+            >una letra
+            >signo de dolar $
+            >caracter de subrayado
+        */
     }
+    
 }
