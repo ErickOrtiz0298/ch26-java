d0fccc03426f840a6db489d7faea6f15dff41812
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-05-17 17:35:59 -0600

    creacion de clase pokemon y pokemon test

--- java07-funciones/src/org/generation/Pokemon.java
+++ java07-funciones/src/org/generation/Pokemon.java
@@ -1,4 +1,25 @@
 package org.generation;
 
 public class Pokemon {
+    //atributos de instancia (non-static fields)
+    String nombre;
+    int alturaCm;
+    double peso;
+    //atributos de clase (static-fields)
+    static String nacimiento = "Oviparos";
+
+    //Metodos constructores
+    //Debe tener el mismo nombre de mi Clase
+    //los métodos constructores no tienen retorno(return)
+
+    Pokemon(String nombre, int alturaCm, double peso){
+        //System.out.println("Has creado un pokemon llamado " + nombre);
+        this.nombre = nombre;
+        this.alturaCm = alturaCm;
+        this.peso = peso;
+
+    }
+
+    //Metodos de instancia
+    //Métodos de clase
 }
